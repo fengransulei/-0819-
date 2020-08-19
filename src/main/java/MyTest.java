7513f92a6a2b001a8bbd9ca836b625932a835dbc
fengran <fengran@hulupos.com>
2020-08-19 16:13:17 +0800

    第一次提交

---
+++ src/main/java/MyTest.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
+public class MyTest {
+    public static void main(String args[]) {
+
+        System.out.println("111111");
+    }
+
+    @Test
+    public void a() {
+
+        System.out.println("222222222");
+    }
+
+
+
+
+}
